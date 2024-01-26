927a7dba284fcc0494fba37d5f895a858e0d5587
Crowdbotics <team@crowdbotics.com>
2024-01-26 21:23:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nvidia-creative-too-45614",
+  url: "https://nvidia-creative-too-45614.botics.co",
+  api: "https://nvidia-creative-too-45614.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
